843fcd9547c238bbbb723f0264feedcd87572879
Izabel Cristina <115845859+Izabel-Cristina@users.noreply.github.com>
2022-11-05 01:36:41 -0300

    Add files via upload

--- agenda.py
+++ agenda.py
@@ -3,21 +3,64 @@ import sqlite3
 from sqlite3 import Error
 
 ##Conexão
-conn = sqlite3.connect('agenda.db')
-cursor = conn.cursor()
+def conexaoBanco():
+    caminho = 'C:\\Users\\usuario\\Desktop\\Python\Projeto\\agenda.db'
+    con = None
+    try:
+        con = sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    return con
 
-##Dados do usuário
+vcon = conexaoBanco()
 
-nome = input('Nome ')
-telefone = input('Telefone ')
-email = input('Email ')
+def menuPrincipal():
+    os.system("csl")
+    print('1 - Inserir Novo Contato')
+    print('2 - Deletar Contato')
+    print('3 - Atualizar Contato ')
+    print('4 - Consultar registro por Nome')
+    print('5 - Sair')
 
-cursor.execute('''
-INSERT INTO agenda (nome, telefone, email)
-Values(?,?,?)''', (nome, telefone, email))
-conn.commit()
+def menuInserir():
+    print()
 
-print('Dados Inseridos Com Sucesso! ')
+def menuDeletar():
+    print()
+ 
+def menuAtualizar():
+    print()
 
-conn.close()
+def menuConsultar():
+    print()
+
+opc = 0
+while opc!=6:
+    menuPrincipal()
+    opc = int(input('Digite uma opção:'))
+    if opc == 1:
+        menuInserir()
+    elif opc==2:
+        menuDeletar()
+    elif opc==3:
+        menuAtualizar()
+    elif opc==4:
+        menuConsultar()
+    elif opc ==5:
+        os.system('cls')
+        print('Programa Finalizado. ')
+    else:
+        os.system('cls')
+        print('opção invalida. ')
+        os.system('pause')
+os.system('pause')
+
+    
+
+
+
+
+
+
+
 
